9d3ad3a00fccab33750239aa9b6594248740fda0
Glen Wallace <wallaceglenw@gmail.com>
2018-10-13 14:13:04 -0500

    Add trigonometric function tests

--- src/test/kotlin/com/wallace/machinelearning/mlmvn/ComplexTest.kt
+++ src/test/kotlin/com/wallace/machinelearning/mlmvn/ComplexTest.kt
@@ -117,4 +117,30 @@ class ComplexTest {
         val expected = Complex(12.0 / 13.0, -18.0 / 13.0)
         assertEquals(expected, div)
     }
+
+    @Test
+    fun sin() {
+        val sin = Complex.sin(complex1)
+        val expected = Complex(9.15449914691142957346729954460983255915886056876518297789, -4.16890695996656435075481305885375484357356560475805588996)
+        assertEquals(expected, sin, accuracyDelta)
+    }
+
+    @Test
+    fun cos() {
+        val cos = Complex.cos(complex1)
+        val expected = Complex(-4.1896256909688072301325550196159737286219454041279210357, -9.1092278937553365979791972627788621213326202389201695649)
+        assertEquals(expected, cos, accuracyDelta)
+    }
+
+    @Test
+    fun exp() {
+        val exp = Complex.exp(complex1)
+        val expected = Complex(-7.3151100949011025174865361510507893218698794489446322367, 1.0427436562359044141015039404625521939183300604422348975)
+        assertEquals(expected, exp, accuracyDelta)
+    }
+
+    fun assertEquals(expected: Complex, actual: Complex, delta: Double) {
+        assertEquals("Real component is inaccurate.", expected.real, actual.real, delta)
+        assertEquals("Imaginary component is inaccurate.", expected.imaginary, actual.imaginary, delta)
+    }
 }
